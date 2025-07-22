2bb89ab7b905d4068b74ded2011d4f951936ae5d
Berkay Ates <atesberkay2356@gmail.com>
2025-07-22 23:28:26 +0300

    refactor: simplify ProductService by removing unused authentication headers

--- webapp-frontend/src/app/core/services/product/product.ts
+++ webapp-frontend/src/app/core/services/product/product.ts
@@ -1,9 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../../models/product.models';
 import { EndpointConstant } from '../../../static/constants/endpoints';
 import { firstValueFrom } from 'rxjs';
-import { LOCAL_STORAGE_KEYS } from '../../constants/keys';
 
 @Injectable({
   providedIn: 'root'
@@ -11,78 +10,41 @@ import { LOCAL_STORAGE_KEYS } from '../../constants/keys';
 export class ProductService {
   http = inject(HttpClient);
 
-  constructor() {
-  }
+  constructor() { }
 
   async createProduct(productValues: Product): Promise<Product> {
-    let url: string = `${EndpointConstant.PRODUCT.CREATE_PRODUCT}`
-    const headers = this.getAuthHeaders();
-
-    const response$ = this.http.post<Product>(url, productValues, { headers });
-    const product = await firstValueFrom(response$);
-    return product;
+    const url = `${EndpointConstant.PRODUCT.CREATE_PRODUCT}`;
+    const response$ = this.http.post<Product>(url, productValues);
+    return await firstValueFrom(response$);
   }
 
   async getProducts(): Promise<Array<Product>> {
-    let url: string = `${EndpointConstant.PRODUCT.GET_PRODUCTS}`
-    const headers = this.getAuthHeaders();
-
-    const response$ = this.http.get<Array<Product>>(url, { headers });
-    const products = await firstValueFrom(response$);
-    return products;
-
+    const url = `${EndpointConstant.PRODUCT.GET_PRODUCTS}`;
+    const response$ = this.http.get<Array<Product>>(url);
+    return await firstValueFrom(response$);
   }
 
   async getProduct(id: number): Promise<Product> {
-    let url: string = `${EndpointConstant.PRODUCT.GET_PRODUCT}/${id}`
-    const headers = this.getAuthHeaders();
-
-    const response$ = this.http.get<Product>(url, { headers });
-    const product = await firstValueFrom(response$);
-    return product;
+    const url = `${EndpointConstant.PRODUCT.GET_PRODUCT}/${id}`;
+    const response$ = this.http.get<Product>(url);
+    return await firstValueFrom(response$);
   }
 
-
   async updateProduct(productValues: Product, id: number): Promise<Product> {
-    let url: string = `${EndpointConstant.PRODUCT.UPDATE_PRODUCT}/${id}`
-    const headers = this.getAuthHeaders();
-
-    const response$ = this.http.post<Product>(url, productValues, { headers });
-    const product = await firstValueFrom(response$);
-    return product;
-
+    const url = `${EndpointConstant.PRODUCT.UPDATE_PRODUCT}/${id}`;
+    const response$ = this.http.post<Product>(url, productValues);
+    return await firstValueFrom(response$);
   }
 
   async patchProduct(productValues: Product, id: number): Promise<Product> {
-    let url: string = `${EndpointConstant.PRODUCT.PATCH_PRODUCT}/${id}`
-    const headers = this.getAuthHeaders();
-
-    const response$ = this.http.patch<Product>(url, productValues, { headers });
-    const product = await firstValueFrom(response$);
-    return product;
+    const url = `${EndpointConstant.PRODUCT.PATCH_PRODUCT}/${id}`;
+    const response$ = this.http.patch<Product>(url, productValues);
+    return await firstValueFrom(response$);
   }
 
   async deleteProduct(id: number): Promise<Product> {
-    let url: string = `${EndpointConstant.PRODUCT.DELETE_PRODUCT}/${id}`
-    const headers = this.getAuthHeaders();
-
-    const response$ = this.http.post<Product>(url, { headers });
-    const product = await firstValueFrom(response$);
-    return product;
+    const url = `${EndpointConstant.PRODUCT.DELETE_PRODUCT}/${id}`;
+    const response$ = this.http.post<Product>(url, {});
+    return await firstValueFrom(response$);
   }
-
-  private getAuthHeaders(): HttpHeaders {
-    const userStr = localStorage.getItem(LOCAL_STORAGE_KEYS.user);
-    const token = userStr ? JSON.parse(userStr)?.accessToken : null;
-
-    if (!token) {
-      throw new Error('There is no valid user');
-    }
-
-    return new HttpHeaders({
-      'Authorization': `Bearer ${token}`,
-      'Content-Type': 'application/json'
-    });
-  }
-
 }
