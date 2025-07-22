a825a3e49311836611ce49457e4f6389c6488c90
Berkay Ates <atesberkay2356@gmail.com>
2025-07-22 19:05:52 +0300

    refactor: remove unused fields from CategoryDTO

--- webapp_api/src/main/java/com/webapp/webapp_api/dto/category/CategoryDTO.java
+++ webapp_api/src/main/java/com/webapp/webapp_api/dto/category/CategoryDTO.java
@@ -1,7 +1,5 @@
 package com.webapp.webapp_api.dto.category;
 
-import java.time.LocalDateTime;
-
 import com.webapp.webapp_api.utils.ProductCategory;
 
 import lombok.Data;
@@ -12,11 +10,7 @@ import lombok.Setter;
 @Setter
 @Data
 public class CategoryDTO {
-    private Long id; 
-
     private ProductCategory productCategory;
-
-    private LocalDateTime createdAt;
 }
 
 
