863d108b6a312683e9990cb51522fa188984a5f8
Berkay Ates <atesberkay2356@gmail.com>
2025-07-24 20:14:34 +0300

    feat: implement seller dashboard with product listing, add, and delete functionality

--- webapp-frontend/src/app/features/seller-dashboard/seller-dashboard.ts
+++ webapp-frontend/src/app/features/seller-dashboard/seller-dashboard.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../core/services/product/product';
+import { Product } from '../../core/models/product.models';
+import { MatDialog } from '@angular/material/dialog';
+import { AddProductDialogComponent } from '../add-product-dialog/add-product-dialog';
 
 @Component({
   selector: 'app-seller-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './seller-dashboard.html',
-  styleUrl: './seller-dashboard.scss'
+  styleUrls: ['./seller-dashboard.scss']
 })
-export class SellerDashboard {
+
+export class SellerDashboard implements OnInit {
+  productService = inject(ProductService);
+  dialog = inject(MatDialog);
+  products: Product[] = [];
+
+  async ngOnInit(): Promise<void> {
+    this.products = await this.productService.getProducts();
+  }
+
+  async goToAddProduct() {
+    const dialogRef = this.dialog.open(AddProductDialogComponent, {
+      width: '400px',
+    });
+
+    const result = await dialogRef.afterClosed().toPromise();
+    console.log('Dialog kapandı, dönen veri:', result);
+    if (result != null) {
+      this.products = await this.productService.getProducts();
+    }
+  }
+
+  async deleteProduct(productId: bigint) {
+    const res = await this.productService.deleteProduct(productId);
+
+    if (res == null) {
+      this.products = await this.productService.getProducts();
+    }
+  }
 
 }
