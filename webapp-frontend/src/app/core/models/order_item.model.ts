51349b0434e18d4c9f1ef78aac69da0dbe161edd
Berkay Ates <atesberkay2356@gmail.com>
2025-07-24 20:10:53 +0300

    fix: correct formatting of OrderItem interface for consistency

--- webapp-frontend/src/app/core/models/order_item.model.ts
+++ webapp-frontend/src/app/core/models/order_item.model.ts
@@ -1,9 +1,10 @@
-export interface OrderItem{
+export interface OrderItem {
     id: bigint;
     created_at: Date;
     quantity: bigint;
     unit_price: bigint;
     updated_at: Date;
     order_id: bigint;
+    category: string;
     product_id: bigint;
 }
