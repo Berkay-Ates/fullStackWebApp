26c5d9f2ae5cee1369b8f12ef520b0106503d8c0
Berkay Ates <atesberkay2356@gmail.com>
2025-07-24 20:05:49 +0300

    refactor: remove Category-related classes and controller for code simplification

--- webapp_api/src/main/java/com/webapp/webapp_api/repository/category/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.webapp.webapp_api.repository.category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.webapp.webapp_api.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    
-}
