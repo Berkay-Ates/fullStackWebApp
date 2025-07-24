1b34913487307c007e67f290cf1e283ab56e6fb7
Berkay Ates <atesberkay2356@gmail.com>
2025-07-24 20:13:45 +0300

    fix: ensure consistent formatting in UserData interface

--- webapp-frontend/src/app/core/storage/localStorage/model.ts
+++ webapp-frontend/src/app/core/storage/localStorage/model.ts
@@ -1,9 +1,10 @@
 import { UserType } from "../../../static/enums/user_types";
 
 export interface UserData {
+    userId: number;
     accessToken: string;
     userType: UserType;
     email: string;
     refreshToken?: string;
-    expiresAt?: number; 
+    expiresAt?: number;
 };
