bf33307077e2dd574f1dc59f625e544fe440a175
Berkay Ates <atesberkay2356@gmail.com>
2025-07-28 20:25:19 +0300

    refactor: simplify response variable naming in ProductService methods

--- webapp-frontend/src/app/core/services/product/product.ts
+++ webapp-frontend/src/app/core/services/product/product.ts
@@ -14,37 +14,37 @@ export class ProductService {
 
   async createProduct(productValues: Product): Promise<Product> {
     const url = `${EndpointConstant.PRODUCT.CREATE_PRODUCT}`;
-    const response$ = this.http.post<Product>(url, productValues);
-    return await firstValueFrom(response$);
+    const response = this.http.post<Product>(url, productValues);
+    return await firstValueFrom(response);
   }
 
   async getProducts(): Promise<Array<Product>> {
     const url = `${EndpointConstant.PRODUCT.GET_PRODUCTS}`;
-    const response$ = this.http.get<Array<Product>>(url);
-    return await firstValueFrom(response$);
+    const response = this.http.get<Array<Product>>(url);
+    return await firstValueFrom(response);
   }
 
   async getProduct(id: number): Promise<Product> {
     const url = `${EndpointConstant.PRODUCT.GET_PRODUCT}/${id}`;
-    const response$ = this.http.get<Product>(url);
-    return await firstValueFrom(response$);
+    const response = this.http.get<Product>(url);
+    return await firstValueFrom(response);
   }
 
   async updateProduct(productValues: Product, id: number): Promise<Product> {
     const url = `${EndpointConstant.PRODUCT.UPDATE_PRODUCT}/${id}`;
-    const response$ = this.http.post<Product>(url, productValues);
-    return await firstValueFrom(response$);
+    const response = this.http.post<Product>(url, productValues);
+    return await firstValueFrom(response);
   }
 
   async patchProduct(productValues: Product, id: number): Promise<Product> {
     const url = `${EndpointConstant.PRODUCT.PATCH_PRODUCT}/${id}`;
-    const response$ = this.http.patch<Product>(url, productValues);
-    return await firstValueFrom(response$);
+    const response = this.http.patch<Product>(url, productValues);
+    return await firstValueFrom(response);
   }
 
   async deleteProduct(id: bigint): Promise<Product> {
     const url = `${EndpointConstant.PRODUCT.DELETE_PRODUCT}/${id}`;
-    const response$ = this.http.delete<Product>(url, {});
-    return await firstValueFrom(response$);
+    const response = this.http.delete<Product>(url, {});
+    return await firstValueFrom(response);
   }
 }
