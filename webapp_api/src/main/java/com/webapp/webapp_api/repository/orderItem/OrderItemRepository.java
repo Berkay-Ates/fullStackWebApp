911441e4babc7ff82121dc69c792f4a9a6e57ea6
Berkay Ates <atesberkay2356@gmail.com>
2025-07-31 20:21:29 +0300

    fix: add missing import for List in OrderItemRepository

--- webapp_api/src/main/java/com/webapp/webapp_api/repository/orderItem/OrderItemRepository.java
+++ webapp_api/src/main/java/com/webapp/webapp_api/repository/orderItem/OrderItemRepository.java
@@ -1,9 +1,12 @@
 package com.webapp.webapp_api.repository.orderItem;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.webapp.webapp_api.model.Order;
 import com.webapp.webapp_api.model.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-    
+    List<OrderItem> findByOrder(Order order);
 }
